b8ead39c8a1d262a5b96d776b99f2223300ed21a
toan4679 <tooan4679@gmail.com>
2025-11-05 02:11:48 +0700

    update sinh viên

--- app/Http/Controllers/SinhVienController.php
+++ app/Http/Controllers/SinhVienController.php
@@ -7,9 +7,31 @@ use App\Models\DangKyHoc;
 use App\Models\BuoiHoc;
 use App\Helpers\RoleHelper;
 use App\Models\DiemDanh;
+use App\Models\SinhVien;
+use Illuminate\Support\Facades\Log;
 
 class SinhVienController extends Controller
 {
+
+    public function index()
+    {
+        try {
+            $sinhViens = SinhVien::with('lop', 'nganh')->get();
+
+            return response()->json([
+                'success' => true,
+                'total' => $sinhViens->count(),
+                'data' => $sinhViens,
+            ]);
+        } catch (\Exception $e) {
+            Log::error('Lỗi lấy danh sách sinh viên: ' . $e->getMessage());
+            return response()->json([
+                'success' => false,
+                'message' => 'Đã xảy ra lỗi khi lấy danh sách sinh viên.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
     public function dashboard(Request $request)
     {
         $role = RoleHelper::getRole($request->user());
