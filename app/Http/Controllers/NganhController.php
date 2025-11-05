a8bac3bf6b88abd3a37f62ed7044bad5aee3b5f0
toan4679 <tooan4679@gmail.com>
2025-11-05 14:25:40 +0700

    import excel

--- app/Http/Controllers/NganhController.php
+++ app/Http/Controllers/NganhController.php
@@ -1,16 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Validation\Rule;
 
 use App\Models\Nganh;
 
 class NganhController extends BaseCrudController
 {
     protected $model = Nganh::class;
-    protected $searchable = ['tenNganh', 'maSo'];
+    protected $searchable = ['tenNganh','maSo'];
     protected $rulesCreate = [
         'tenNganh' => 'required|string|max:100',
-        'maSo' => ['required', 'string', Rule::unique('nganh', 'maSo')->ignore($id, 'maNganh')],
+        'maSo'     => 'required|string|max:20|unique:nganh,maSo',
     ];
 }
