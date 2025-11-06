1bb68059d48ea624dd0642064f872d6658ef035f
Kiet <contacttuankiet.dev@gmail.com>
2025-11-06 20:50:52 +0700

    Thửu route mới test lophocphan của giảng viên

--- routes/api.php
+++ routes/api.php
@@ -136,6 +136,7 @@ Route::prefix('v1')->group(function () {
     Route::prefix('giangvien')->middleware('auth:sanctum')->group(function () {
         Route::get('/lichday', [LichDayController::class, 'index']);
         Route::get('/lophocphan', [LopHocPhanController::class, 'byGiangVien']);
+        Route::get('/lophocphan2', [GiangVienController::class, 'getLopHocPhan']);
         Route::get('/', [GiangVienController::class, 'getAll']);
         Route::get('/{id}', [GiangVienController::class, 'getDetail']);
         // QR điểm danh
